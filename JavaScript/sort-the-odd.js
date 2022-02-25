b60043eb391293aeee4a6b330436807ada3486ae
Błażej Domagała <petitoff@outlook.com>
2022-02-25 15:48:57 +0100

    Create sort-the-odd.js

---
+++ JavaScript/sort-the-odd.js
@@ -0,0 +1,24 @@
+function sortArray(array) {
+  oddArray = [];
+  finalArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const number = array[i];
+    if (number % 2 !== 0) {
+      oddArray.push(number);
+    }
+  }
+  const sortedNumbers = [...new Float64Array(oddArray).sort()];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 !== 0) {
+      finalArray.push(sortedNumbers.shift());
+    } else {
+      finalArray.push(array[i]);
+    }
+  }
+  return finalArray;
+}
+
+const array = [11, 1, 2, 8, 3, 4, 5];
+
+console.log(sortArray(array));
