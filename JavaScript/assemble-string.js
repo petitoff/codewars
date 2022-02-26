719c9c259f6891f4adb0f11000d095138fc5f929
Błażej Domagała <petitoff@outlook.com>
2022-02-26 13:10:11 +0100

    Create assemble-string.js

---
+++ JavaScript/assemble-string.js
@@ -0,0 +1,26 @@
+function assembleString(array) {
+  if (typeof array === "undefined" || !array.length > 0) return "";
+  let stringArray = array[0];
+
+  for (let i = 1; i < array.length; i++) {
+    for (let j = 0; j < stringArray.length; j++) {
+      const letter = array[i][j];
+      if (letter !== "*") {
+        stringArray = stringArray.split("");
+        stringArray[j] = letter;
+        stringArray = stringArray.join("");
+      }
+    }
+  }
+  stringArray = stringArray.split("");
+  for (let i = 0; i < stringArray.length; i++) {
+    if (stringArray[i] === "*") {
+      stringArray[i] = "#";
+    }
+  }
+
+  return stringArray.join("");
+}
+
+input = [];
+console.log(assembleString(input));
