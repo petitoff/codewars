6f446e71102158b1915c7c86abefcbd20b077b95
Błażej Domagała <petitoff@outlook.com>
2022-03-08 15:43:52 +0100

    Create shiftedDiff.js

---
+++ JavaScript/shiftedDiff.js
@@ -0,0 +1,20 @@
+function shiftedDiff(first, second) {
+  console.log(first, second);
+  if (first === second) {
+    return 0;
+  }
+
+  const firstArray = first.split("");
+
+  for (let i = 0; i < firstArray.length; i++) {
+    const lastElement = firstArray.pop();
+    firstArray.unshift(lastElement);
+    if (firstArray.join("") === second) {
+      console.log(firstArray);
+      return i + 1;
+    }
+  }
+  return -1;
+}
+
+console.log(shiftedDiff("eecoff", "coffee"));
